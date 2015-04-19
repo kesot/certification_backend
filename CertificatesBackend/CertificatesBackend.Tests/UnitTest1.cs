d7e733cc65871db9aeab041efa4c3f7b6cb19ad6
kesot <kesot@mail.ru>
2015-04-19 23:06:46 +0300

    Service
    Service and test templates

---
+++ CertificatesBackend/CertificatesBackend.Tests/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+using System.Web.Script.Serialization;
+using CertificatesBackend.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CertificatesBackend.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var controller = new TestController
+            {
+                Request = new HttpRequestMessage(),
+                Configuration = new HttpConfiguration()
+            };
+
+            // Act
+            var response = ((OkNegotiatedContentResult<string[]>)controller.Get()).Content;
+
+            Assert.AreEqual("value1", response[0]);
+            // Assert
+           
+
+        }
+    }
+}

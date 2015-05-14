898db13a7c563e506584af5040c131668957ecdb
yurochko yura <yurochkoyura@gmail.com>
2015-05-15 00:36:05 +0300

    refactoring users_backend handlers + tests

--- users/users_backend.py
+++ users/users_backend.py
@@ -1,5 +1,5 @@
 from db_processor import DBProcessor
-from db_tables import Users
+from db_tables import Users, Clients
 from tornado.options import define, options, parse_command_line
 from defines import USERS_PORT, LOG_FORMAT, LOG_USERS_FNAME
 import tornado.ioloop
@@ -12,55 +12,97 @@ define("debug", default=True)
 
 class AddUserHandler(tornado.web.RequestHandler):
     def post(self):
-        data = tornado.escape.json_decode(self.request.body)
-        user = Users(data)
-        if db.add_user(user):
-            self.write(json.dumps({"answer": 1}))
-        else:
-            logging.error("add_user request: {0}".format(str(data)))
-            self.write(json.dumps({"answer": 0, "error": "Can't add new user"}))
+        try:
+            data = tornado.escape.json_decode(self.request.body)
+            if not "type" in data:
+                raise Exception("Argument 'type' required")
+
+            entity_type = int(data["type"])
+            if entity_type == 0:
+                entity = Users(data)
+            elif entity_type == 1:
+                entity = Clients(data)
+            else:
+                raise Exception("Unknown value for 'type' argument")
+
+            if db.add_entity(entity):
+                self.write(json.dumps({"answer": 1}))
+            else:
+                raise Exception("Can't add new user")
+
+        except Exception as e:
+            logging.error("add_user request: {0}".format(str(e)))
+            self.set_status(400)
+            self.write(json.dumps({"answer": 0, "error": str(e)}))
 
 
 class GetUserHandler(tornado.web.RequestHandler):
     def get(self):
-        login = self.get_argument("login")
-        user = db.get_user(login)
-        if not user:
-            message = "Can't find such user {0}".format(login)
-            logging.error("get_user request: {0}".format(message))
-            self.write(json.dumps({"answer": 0, "error": message}))
-        else:
-            user_json = user.to_json()
-            user_json["answer"] = 1
-            self.write(json.dumps(user_json, ensure_ascii=False))
+        try:
+            login = self.get_argument("login")
+            entity_type = int(self.get_argument("type"))
+
+            entity = db.get_entity(login, entity_type)
+            if not entity:
+                raise Exception("Can't find such user {0}".format(login))
+            else:
+                user_json = entity.to_json()
+                user_json["answer"] = 1
+                self.write(json.dumps(user_json, ensure_ascii=False))
+
+        except Exception as e:
+            logging.error("get_user request: {0}".format(str(e)))
+            self.set_status(400)
+            self.write(json.dumps({"answer": 0, "error": str(e)}))
 
 
 class RemoveUserHandler(tornado.web.RequestHandler):
     def delete(self):
-        data = tornado.escape.json_decode(self.request.body)
-        login = data["login"]
-        user = db.get_user(login)
-        if not user:
-            message = "Can't find such user {0}".format(login)
-            logging.error("remove_user request: {0}".format(message))
-            self.write(json.dumps({"answer": 0, "error": message}))
-        else:
-            if db.remove_user(user):
-                self.write({"answer": 1})
+        try:
+            data = tornado.escape.json_decode(self.request.body)
+            if not "login" in data:
+                raise Exception("Argument 'login' required")
+            if not "type" in data:
+                raise Exception("Argument 'type' required")
+
+            login = data["login"]
+            entity_type = int(data["type"])
+
+            entity = db.get_entity(login, entity_type)
+            if not entity:
+                raise Exception("Can't find user with login {0}".format(login))
             else:
-                message = "Can't remove such user {0}".format(login)
-                logging.error("remove_user request: {0}".format(message))
-                self.write({"answer": 0, "error": message})
+                if db.remove_entity(entity):
+                    self.write({"answer": 1})
+                else:
+                    raise Exception("Can't remove user with login {0}".format(login))
+
+        except Exception as e:
+            logging.error("remove_user request: {0}".format(str(e)))
+            self.set_status(400)
+            self.write(json.dumps({"answer": 0, "error": str(e)}))
 
 
 class UpdateUserHandler(tornado.web.RequestHandler):
     def put(self):
-        data = tornado.escape.json_decode(self.request.body)
-        if not db.update_user(data):
-            logging.error("update_user request: {0}".format(str(data)))
-            self.write(json.dumps({"answer": 0, "error": "Can't update such user"}))
-        else:
-            self.write(json.dumps({"answer": 1}))
+        try:
+            data = tornado.escape.json_decode(self.request.body)
+            if not "login" in data:
+                raise Exception("Argument 'login' required")
+            if not "type" in data:
+                raise Exception("Argument 'type' required")
+
+            entity_type = int(data["type"])
+
+            if not db.update_entity(data, entity_type):
+                raise Exception("Can't update user with login {0}".format(data["login"]))
+            else:
+                self.write(json.dumps({"answer": 1}))
+
+        except Exception as e:
+            logging.error("update_user request: {0}".format(str(e)))
+            self.set_status(400)
+            self.write(json.dumps({"answer": 0, "error": str(e)}))
 
 
 if __name__ == "__main__":
--- users/users_backend_tests.py
+++ users/users_backend_tests.py
@@ -19,81 +19,152 @@ class TestUsersBackendMethods(unittest.TestCase):
         self.update_method_url = users_url + "/update_user"
 
     def test_add_bad_method(self):
-        """
-        the password-field is missing
-        """
         data = {"fname": "Иван", "sname": "Иванов", "mname": "Иванович",
                 "birthday": "1990-11-11", "email": "ivanov@test.com",
-                "login": "ivan"}
+                "login": "ivan", "password": self.password}
         headers = {"Content-type": "application/json"}
+        result = requests.post(self.add_method_url, data=json.dumps(data), headers=headers)
+        self.assertEqual(result.json()["answer"], 0)
+
+        data["type"] = 3
         result = requests.post(self.add_method_url, data=json.dumps(data), headers=headers)
         self.assertEqual(result.json()["answer"], 0)
 
     def test_remove_bad_method(self):
-        """
-        the login 'test_user' not exist
-        """
-        data = {"login": "test_user"}
+        data = {}
         headers = {"Content-type": "application/json"}
         result = requests.delete(self.remove_method_url, data=json.dumps(data), headers=headers)
         self.assertEqual(result.json()["answer"], 0)
+        data["login"] = "test"
+        result = requests.delete(self.remove_method_url, data=json.dumps(data), headers=headers)
+        self.assertEqual(result.json()["answer"], 0)
+        data["type"] = 0
+        result = requests.delete(self.remove_method_url, data=json.dumps(data), headers=headers)
+        self.assertEqual(result.json()["answer"], 0)
+        data["type"] = 1
+        result = requests.delete(self.remove_method_url, data=json.dumps(data), headers=headers)
+        self.assertEqual(result.json()["answer"], 0)
+
+    def test_user_add_remove_method(self):
+        data = {"fname": "Иван", "sname": "Иванов", "mname": "Иванович",
+                "birthday": "1990-11-11", "email": "ivanov@test.com",
+                "login": "ivan", "password": self.password, "type": 0}
+        headers = {"Content-type": "application/json"}
+        result = requests.post(self.add_method_url, data=json.dumps(data), headers=headers)
+        self.assertEqual(result.json()["answer"], 1)
+        result = requests.post(self.add_method_url, data=json.dumps(data), headers=headers)
+        self.assertEqual(result.json()["answer"], 0)
+        data = {"login": "ivan", "type": 1}
+        result = requests.delete(self.remove_method_url, data=json.dumps(data), headers=headers)
+        self.assertEqual(result.json()["answer"], 0)
+        data["type"] = 0
+        result = requests.delete(self.remove_method_url, data=json.dumps(data), headers=headers)
+        self.assertEqual(result.json()["answer"], 1)
+
+    def test_client_add_remove_method(self):
+        data = {"login": "ozon", "password": self.password, "type": 0}
+        headers = {"Content-type": "application/json"}
+        result = requests.post(self.add_method_url, data=json.dumps(data), headers=headers)
+        self.assertEqual(result.json()["answer"], 0)
+        data["type"] = 1
+        result = requests.post(self.add_method_url, data=json.dumps(data), headers=headers)
+        self.assertEqual(result.json()["answer"], 1)
+        result = requests.post(self.add_method_url, data=json.dumps(data), headers=headers)
+        self.assertEqual(result.json()["answer"], 0)
+        data = {"login": "ozon", "type": 0}
+        result = requests.delete(self.remove_method_url, data=json.dumps(data), headers=headers)
+        self.assertEqual(result.json()["answer"], 0)
+        data["type"] = 1
+        result = requests.delete(self.remove_method_url, data=json.dumps(data), headers=headers)
+        self.assertEqual(result.json()["answer"], 1)
 
     def test_get_bad_method(self):
-        """
-        the login 'test_user' not exist
-        """
-        result = requests.get(self.get_method_url+"?login=test_user")
+        result = requests.get(self.get_method_url)
+        self.assertEqual(result.json()["answer"], 0)
+        result = requests.get(self.get_method_url + "?login=test")
+        self.assertEqual(result.json()["answer"], 0)
+        result = requests.get(self.get_method_url + "?login=test&type=0")
+        self.assertEqual(result.json()["answer"], 0)
+        result = requests.get(self.get_method_url + "?login=test&type=1")
         self.assertEqual(result.json()["answer"], 0)
 
-    def test_update_bad_method(self):
-        """
-        the login 'test_user' not exist
-        """
-        data = {"login": "test_user"}
-        headers = {"Content-type": "application/json"}
-        result = requests.put(self.update_method_url, data=json.dumps(data), headers=headers)
-        self.assertEqual(result.json()["answer"], 0)
-
-    def test_add_remove_method(self):
+    def test_user_add_get_method(self):
         data = {"fname": "Иван", "sname": "Иванов", "mname": "Иванович",
                 "birthday": "1990-11-11", "email": "ivanov@test.com",
-                "login": "ivan", "password": self.password}
-        headers = {"Content-type": "application/json"}
-        result = requests.post(self.add_method_url, data=json.dumps(data), headers=headers)
-        self.assertEqual(result.json()["answer"], 1)
-        result = requests.post(self.add_method_url, data=json.dumps(data), headers=headers)
-        self.assertEqual(result.json()["answer"], 0)
-        data = {"login": "ivan"}
-        result = requests.delete(self.remove_method_url, data=json.dumps(data), headers=headers)
-        self.assertEqual(result.json()["answer"], 1)
-
-    def test_get_method(self):
-        data = {"fname": "Иван", "sname": "Иванов", "mname": "Иванович",
-                "birthday": "1990-11-11", "email": "ivanov@test.com",
-                "login": "ivan", "password": self.password}
+                "login": "ivan", "password": self.password, "type": 0}
         headers = {"Content-type": "application/json"}
         result = requests.post(self.add_method_url, data=json.dumps(data), headers=headers)
         self.assertEqual(result.json()["answer"], 1)
         data["answer"] = 1
-        result = requests.get(self.get_method_url+"?login=ivan").json()
+        result = requests.get(self.get_method_url+"?login=ivan&type=1").json()
+        self.assertEqual(result["answer"], 0)
+        result = requests.get(self.get_method_url+"?login=ivan&type=0").json()
+        result["type"] = 0
         self.assertDictEqual(data, result)
-        data = {"login": "ivan"}
+        data = {"login": "ivan", "type": 0}
         result = requests.delete(self.remove_method_url, data=json.dumps(data), headers=headers)
         self.assertEqual(result.json()["answer"], 1)
 
-    def test_update_method(self):
-        data = {"fname": "Иван", "sname": "Иванов", "mname": "Иванович",
-                "birthday": "1990-11-11", "email": "ivanov@test.com",
-                "login": "ivan", "password": self.password}
+    def test_client_add_get_method(self):
+        data = {"login": "ozon", "password": self.password, "type": 1}
         headers = {"Content-type": "application/json"}
         result = requests.post(self.add_method_url, data=json.dumps(data), headers=headers)
         self.assertEqual(result.json()["answer"], 1)
-        data = {"fname": "Андрей", "sname": "Андреев", "mname": "Андреевич",
-                "birthday": "1991-12-12", "email": "andreev@test.com",
-                "login": "ivan", "password": self.password}
+        data["answer"] = 1
+        result = requests.get(self.get_method_url+"?login=ozon&type=0").json()
+        self.assertEqual(result["answer"], 0)
+        result = requests.get(self.get_method_url+"?login=ozon&type=1").json()
+        result["type"] = 1
+        self.assertDictEqual(data, result)
+        data = {"login": "ozon", "type": 1}
+        result = requests.delete(self.remove_method_url, data=json.dumps(data), headers=headers)
+        self.assertEqual(result.json()["answer"], 1)
+
+    def test_update_bad_method(self):
+        data = {}
+        headers = {"Content-type": "application/json"}
+        result = requests.put(self.update_method_url, data=json.dumps(data), headers=headers)
+        self.assertEqual(result.json()["answer"], 0)
+        data["login"] = "test"
+        result = requests.put(self.update_method_url, data=json.dumps(data), headers=headers)
+        self.assertEqual(result.json()["answer"], 0)
+        data["type"] = 0
+        result = requests.put(self.update_method_url, data=json.dumps(data), headers=headers)
+        self.assertEqual(result.json()["answer"], 0)
+        data["type"] = 1
+        result = requests.put(self.update_method_url, data=json.dumps(data), headers=headers)
+        self.assertEqual(result.json()["answer"], 0)
+
+    def test_user_update_method(self):
+        data = {"fname": "Иван", "sname": "Иванов", "mname": "Иванович",
+                "birthday": "1990-11-11", "email": "ivanov@test.com",
+                "login": "ivan", "password": self.password, "type": 0}
+        headers = {"Content-type": "application/json"}
+        result = requests.post(self.add_method_url, data=json.dumps(data), headers=headers)
+        self.assertEqual(result.json()["answer"], 1)
+        data = {"fname": "Андрей", "sname": "Андреев", "mname": "Андреевич", "login": "ivan", "type": 0}
         result = requests.put(self.update_method_url, data=json.dumps(data), headers=headers)
         self.assertEqual(result.json()["answer"], 1)
-        data = {"login": "ivan"}
+        result = requests.get(self.get_method_url + "?login=ivan&type=0").json()
+        self.assertEqual(result["answer"], 1)
+        self.assertEqual(result["fname"], "Андрей")
+        self.assertEqual(result["sname"], "Андреев")
+        data = {"login": "ivan", "type": 0}
+        result = requests.delete(self.remove_method_url, data=json.dumps(data), headers=headers)
+        self.assertEqual(result.json()["answer"], 1)
+
+    def test_client_update_method(self):
+        data = {"login": "ozon", "password": self.password, "type": 1}
+        headers = {"Content-type": "application/json"}
+        result = requests.post(self.add_method_url, data=json.dumps(data), headers=headers)
+        self.assertEqual(result.json()["answer"], 1)
+        data = {"password": "new_password", "login": "ozon", "type": 1}
+        result = requests.put(self.update_method_url, data=json.dumps(data), headers=headers)
+        self.assertEqual(result.json()["answer"], 1)
+        result = requests.get(self.get_method_url + "?login=ozon&type=1").json()
+        self.assertEqual(result["answer"], 1)
+        self.assertEqual(result["password"], "new_password")
+        data = {"login": "ozon", "type": 1}
         result = requests.delete(self.remove_method_url, data=json.dumps(data), headers=headers)
         self.assertEqual(result.json()["answer"], 1)
 
